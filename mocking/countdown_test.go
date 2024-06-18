4565d2d6372da44367789765efb58e1b68811694
Raphael Tavares <raphael_tavares2000@hotmail.com>
2024-06-18 00:03:00 -0300

    feat: mocking section done

---
+++ mocking/countdown_test.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bytes"
+	"reflect"
+	"testing"
+	"time"
+)
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+const write = "write"
+const sleep = "sleep"
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+func TestCountdown(t *testing.T) {
+	t.Run("print the correct string", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+
+		Countdown(buffer, &SpyCountdownOperations{})
+
+		got := buffer.String()
+		want := "3" + "\n" + "2" + "\n" + "1" + "\n" + "Go!"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("sleep happens before every print", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		want := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
+		}
+	})
+}
+
+func TestConfigurableSleeper(t *testing.T) {
+	sleepTime := 5 * time.Second
+
+	spyTime := &SpyTime{}
+	sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+	sleeper.Sleep()
+
+	if spyTime.durationSlept != sleepTime {
+		t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
+	}
+
+}
---
+++ mocking/go.mod
@@ -0,0 +1,3 @@
+module github.com/RaphaTavares/mocking
+
+go 1.22.4
